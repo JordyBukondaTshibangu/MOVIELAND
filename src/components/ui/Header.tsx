03138b07a83ec9df308269dce06fa5283d832bb4
Jordy Bukonda Tshibangu <69379641+JordyBukondaTshibangu@users.noreply.github.com>
2024-03-15 04:55:12 +0200

    style : configure tailwind setup

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header: React.FC = () => {
+  return (
+    <header>
+      <div>
+        <nav>
+          <div className="text-4xl text-primary">MovieLand</div>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,59 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "selector",
   theme: {
-    extend: {fontFamily: {
-      poppins: ['Poppins', 'sans-serif'],
-  }},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+      "2xl": "1536px",
+      "3xl": "1990px",
+    },
+    colors: {
+      primary: "#FD7401",
+      secondary: "#FF8755",
+      success: "#0BA02C",
+      bgSuccess: "#00B83E",
+      danger: "#dc3545",
+      info: "#738AFF",
+      fontColor: "#BABABF",
+      lightFontColor: "#555",
+      light: "#F4F2EE",
+      // #
+      dark: "#242535",
+      darkHeader: "#181A2A",
+      darkMirage: "#1b1c28",
+      black: "#000",
+      white: "#FFF",
+      beige: "#F5F5DC",
+      lightGray: "#F1F3F6",
+      grayFirst: "#B4B4B8",
+      graySecond: "#B9C0BD",
+      grayThird: "#E3E1D9",
+      grayFour: "#F2EFE5",
+      arsenic: "#3B444B",
+      coral: "#54626F",
+    },
+    fontFamily: {
+      sans: ["Roboto", "Arial", "sans-serif"],
+    },
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      spacing: {
+        "8xl": "96rem",
+        "9xl": "128rem",
+      },
+    },
   },
   plugins: [],
 }
